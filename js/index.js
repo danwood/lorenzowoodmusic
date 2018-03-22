4111f2caa4747ab4078ae46e27665d583c050056
Dan Wood <danwood@users.noreply.github.com>
2018-03-22 12:59:51 -0700

    youtube player

--- js/index.js
+++ js/index.js
@@ -95,6 +95,52 @@ $('#close-redeem').click(function() {
     $('#redeemer').fadeOut('fast');
 });
 
+// From https://webdesign.tutsplus.com/tutorials/how-to-lazy-load-embedded-youtube-videos--cms-26743
+// No jquery needed
+//
+// //               iframe(src="https://www.youtube.com/embed/"+code allowfullscreen=true)
+
+
+var youtube = document.querySelectorAll( ".youtube" );
+for (var i = 0; i < youtube.length; i++) {
+    var embed = youtube[i].dataset.embed.split(':');
+    var code = embed[0];
+    var title = embed[1];
+
+    var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+    if (iOS) {
+      // regular YouTube iframe embed so that it needs just one tap as expected
+      var iframe = document.createElement( "iframe" );
+      iframe.setAttribute( "allowfullscreen", "" );
+      iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ code );
+      youtube[i].appendChild( iframe );
+    }
+    else {
+      var source = "https://img.youtube.com/vi/"+ code +"/sddefault.jpg";
+      var image = new Image();
+      image.src = source;
+      image.addEventListener( "load", function() {
+          youtube[ i ].appendChild( image );
+      }( i ) );
+
+      youtube[i].addEventListener( "click", function() {
+
+          var iframe = document.createElement( "iframe" );
+          iframe.setAttribute( "frameborder", "0" );
+          iframe.setAttribute( "allowfullscreen", "" );
+          iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ code +"?rel=0&showinfo=0&autoplay=1" );
+          this.innerHTML = "";
+          this.appendChild( iframe );
+      } );
+      var play = document.createElement("div");
+      play.setAttribute("class", "play-button");
+      youtube[i].appendChild(play);
+      var t = document.createElement("div");
+      t.setAttribute("class", "title");
+      t.innerText = title;
+      youtube[i].appendChild(t);
+    }
+  };
 
 
 //Modernizer-like
