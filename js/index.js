44013bffdef6fd674a2df6dca670ab9fab94960a
Dan Wood <danwood@users.noreply.github.com>
2020-03-23 11:10:55 -0700

    contact form without javascript on separate page

--- js/index.js
+++ js/index.js
@@ -37,6 +37,34 @@ $('.contact-link').click(function() {
 	return false;
 });
 
+// Since this requires jquery, we're only using this in the home page.
+$('#contact-form').submit(function( event ) {
+	$.ajax({
+		type: 'POST',
+		url: 'https://www.lorenzowoodmusic.com/mailme.php',
+		data: $("#contact-form").serialize(),
+
+		success: function(data, textStatus, jqXHR ) {
+			if (data !== '') {
+				$('#e_fm').val('');
+				$('#e_ms').val('');
+				$('#e_na').val('');
+				$('#close-modal').click();	// might not be found if this is not on the homepage modal
+				setTimeout(function(){ window.alert("Your message was sent. You should hear back from us soon!"); },0);
+			} else {
+				window.alert('Sorry, but the contact form submission did not work as expected.');
+			}
+		},
+		error: function(jqXHR, textStatus, errorThrown ) {
+			window.alert('ERROR. ' + errorThrown + ' ' + textStatus);
+		},
+		complete: function(jqXHR, textStatus ) {
+
+		}
+	});
+	event.preventDefault();
+});
+
 
 $('#redeem-form').submit(function( event ) {
 	if ($('#redeem-input').val().length > 0) {
