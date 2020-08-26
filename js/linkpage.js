44c51aa0e7ac83fff1a02733b2d3b0dc7eabf03b
Dan Wood <danwood@users.noreply.github.com>
2020-08-25 20:25:53 -0700

    use JS to bring in real blur over blurhash

--- js/linkpage.js
+++ js/linkpage.js
@@ -11,3 +11,16 @@ if (blurhashData) {
 		blurhashImage.src = dataUrl;
 	}
 }
+
+var backgroundBlur = document.getElementById('background-blur');
+var blurImage = new Image();
+// When the real image is loaded, display it and hide the blurhash.
+blurImage.onload = function() {
+    var blurhashImage = document.getElementById('blurhashImage');
+    blurhashImage.style.display = 'none';
+    blurImage.style.display = 'block';
+};
+blurImage.style.display = 'none';
+blurImage.src = "<?php $blur = CLOUDPREFIX . ($release['image_blurred'] ? 'blurred_100/' . $release['image_blurred'] : 'album_art_640/' . htmlentities($release['image'])); echo htmlentities($blur, ENT_QUOTES); ?>";
+blurImage.alt = "<?php echo htmlentities($release['title'], ENT_QUOTES); ?>";
+backgroundBlur.appendChild(blurImage);
