5ed3b57b48a441b4366bb04ae496d770f0d401f5
Dan Wood <danwood@users.noreply.github.com>
2020-12-27 16:54:44 -0800

    get background images for each release

--- source/classes/downcode.php
+++ source/classes/downcode.php
@@ -11,6 +11,46 @@ define('DOWNCODE_DBDIR',	PRIVATE_RESOLVED . '/downcode_db');
 
 define('CLOUDPREFIX', 'https://res.cloudinary.com/avfivcfiwkvgmspufqrh/image/upload/lwm/');
 
+/* Convert hexdec color string to rgb(a) string */
+ 
+function hex2rgba($color, $opacity = false) {
+ 
+	$default = 'rgb(0,0,0)';
+ 
+	//Return default if no color provided
+	if(empty($color))
+          return $default; 
+ 
+	//Sanitize $color if "#" is provided 
+        if ($color[0] == '#' ) {
+        	$color = substr( $color, 1 );
+        }
+ 
+        //Check if color has 6 or 3 characters and get values
+        if (strlen($color) == 6) {
+                $hex = array( $color[0] . $color[1], $color[2] . $color[3], $color[4] . $color[5] );
+        } elseif ( strlen( $color ) == 3 ) {
+                $hex = array( $color[0] . $color[0], $color[1] . $color[1], $color[2] . $color[2] );
+        } else {
+                return $default;
+        }
+ 
+        //Convert hexadec to rgb
+        $rgb =  array_map('hexdec', $hex);
+ 
+        //Check if opacity is set(rgba or rgb)
+        if($opacity){
+        	if(abs($opacity) > 1)
+        		$opacity = 1.0;
+        	$output = 'rgba('.implode(",",$rgb).','.$opacity.')';
+        } else {
+        	$output = 'rgb('.implode(",",$rgb).')';
+        }
+ 
+        //Return rgb(a) color string
+        return $output;
+}
+
 
 function baseURL() {
 	$pageURL = 'http';
@@ -173,7 +213,7 @@ class DowncodeDB extends SQLite3
 
 
 	function pathCDNForImageSize($release, $size = 3000, $useCDN = true) {
-		if (!in_array($size, Array(64, 384, 640, '1200x630', 3000, 'blurred'))) return NULL;
+		if (!in_array($size, Array(64, 384, 640, '1200x630', 'background_1200x630', 3000, 'blurred'))) return NULL;
 
 		$cloudPrefix = $useCDN ? CLOUDPREFIX : '/';
 
