051440099245b60f87592ef0b904e2410536fc69
Dan Wood <danwood@users.noreply.github.com>
2021-08-18 08:11:58 -0700

    load spotify embed via a script, need to make lazy loading

---
+++ source/js/spotify.js
@@ -0,0 +1,16 @@
+// Lazily bring in a spotify embed
+
+var spotify = document.querySelectorAll( ".spotify-player" );
+for (var i = 0; i < spotify.length; i++) {
+	var code	= spotify[i].dataset.code;
+
+	var container = document.createElement("div");
+	container.setAttribute("class", "spotify-embed");
+	var iframe = document.createElement( "iframe" );
+	iframe.setAttribute( "frameborder", "0" );
+	iframe.setAttribute( "allowtransparency", "true" );
+	iframe.setAttribute( "allow", "encrypted-media" );
+	iframe.setAttribute( "src", "https://open.spotify.com/embed/track/"+ code );
+	container.appendChild(iframe);
+	spotify[i].replaceWith( container );
+}
