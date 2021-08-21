195a86b4bc72e12d4891662ef852589428a3b54c
Dan Wood <danwood@users.noreply.github.com>
2021-08-20 20:33:10 -0700

    Yay loads spotify only when scrolled to reveal

--- source/js/spotify.js
+++ source/js/spotify.js
@@ -1,19 +1,15 @@
 // Lazily bring in a spotify embed
 
-document.addEventListener('DOMContentLoaded', function(event) {
-	var spotify = document.querySelectorAll( ".spotify-player" );
-	for (var i = 0; i < spotify.length; i++) {
-		var code	= spotify[i].dataset.code;
-		var container = document.createElement("div");
-		container.setAttribute("class", "spotify-embed");
-		var iframe = document.createElement( "iframe" );
-		iframe.setAttribute( "frameborder", "0" );
-		iframe.setAttribute( "loading", "lazy" );	
-		iframe.setAttribute( "allowtransparency", "true" );
-		iframe.setAttribute( "allow", "encrypted-media" );
-		iframe.setAttribute( "src", "https://open.spotify.com/embed/track/"+ code );
-		container.appendChild(iframe);
-		spotify[i].replaceWith( container );
-	}
-})
-
+function loadSpotifyIntoNode(spotifyProxy) {
+	var code	= spotifyProxy.dataset.code;
+	var container = document.createElement("div");
+	container.setAttribute("class", "spotify-embed");
+	var iframe = document.createElement( "iframe" );
+	iframe.setAttribute( "frameborder", "0" );
+	iframe.setAttribute( "loading", "lazy" );	
+	iframe.setAttribute( "allowtransparency", "true" );
+	iframe.setAttribute( "allow", "encrypted-media" );
+	iframe.setAttribute( "src", "https://open.spotify.com/embed/track/"+ code );
+	container.appendChild(iframe);
+	spotifyProxy.replaceWith( container );
+}
