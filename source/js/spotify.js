81c0b91702123f99b98e8da121c311cb3ee3df86
Dan Wood <danwood@users.noreply.github.com>
2022-04-29 21:04:06 -0700

    more misc updates

--- source/js/spotify.js
+++ source/js/spotify.js
@@ -8,7 +8,7 @@ function loadSpotifyIntoNode(spotifyProxy) {
 	iframe.setAttribute( "frameborder", "0" );
 	iframe.setAttribute( "loading", "lazy" );	
 	iframe.setAttribute( "allowtransparency", "true" );
-	iframe.setAttribute( "allow", "encrypted-media" );
+	iframe.setAttribute( "allow", "autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" );
 	iframe.setAttribute( "src", "https://open.spotify.com/embed/track/"+ code );
 	container.appendChild(iframe);
 	spotifyProxy.replaceWith( container );
